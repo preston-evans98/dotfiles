724f4e6c262fd5aa954967ae2748fa1d3bebaad5
Preston Evans <pbevans1@crimson.ua.edu>
2020-03-06 10:12:53 -0600

    tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-ln -s dotfiles/.inputrc ~/.inputrc
-ln -s dotfiles/.vimrc ~/.vimrc
-ln -s ./dotfiles/.bash_profile ~/.bash_profile
 
+
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf 
